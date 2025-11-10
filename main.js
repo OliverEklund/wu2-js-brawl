dda99834ca1925479490a233a2fcf3e1f3ebf75b
OliverEklund <oliver.eklund@elev.ga.ntig.se>
2025-11-10 11:04:46 +0100

    refactor: enhance game logic and update combat log functionality

--- main.js
+++ main.js
@@ -3,31 +3,70 @@ const playerName = "Jens" // statiskt så vi slipper prompt varje körning
 let playerHp = 100
 let enemyHp = 100
 
+const playButton = document.querySelector("#play-button")
+
 function rollDice() {
-    return Math.ceil(Math.random() * 6)
+    return Math.ceil(Math.random() * 20)
 }
 
 const playerHpElement = document.querySelector("#player-hp")
 const enemyHpElement = document.querySelector("#enemy-hp")
+
+function log(message, type) {
+    const li = document.createElement("li")
+    if (type) {3
+        li.classList.add(type)
+    }
+    li.textContent = message
+    combatLogElement.appendChild(li)
+    if (combatLogElement.childNodes.length > 10) {
+        combatLogElement.removeChild(combatLogElement.firstChild)
+    }
+}
 
 function gameRound() {    
     const playerRoll = rollDice()
     const enemyRoll = rollDice()
     if (playerRoll > enemyRoll) {
         const damage = playerRoll - enemyRoll
-        console.log(`Du köttar fienden för ${damage}!`)
+
+        const playerattacks = [
+            `Du träffar han för ${damage} skada!`,
+            `Du slår han i nylet för ${damage} skada!`,
+            `Du sparkar han i magen för ${damage} skada!`,
+            `Du träffar han för ${damage} skada!`,
+            `Du stampar på hans tår för ${damage} skada!`
+        ]
+        
+        log(playerattacks[Math.ceil(Math.random() * playerattacks.length - 1)], "player")
         enemyHp -= damage
     } else if (enemyRoll > playerRoll) {
         const damage = enemyRoll - playerRoll
-        console.log(`Nedrans, du blir mulad för ${damage}!`)
+
+        const enemyattacks = [
+            `Han träffar dig för ${damage} skada!`,
+            `Han slår dig i nylet för ${damage} skada!`,
+            `Han sparkar dig i magen för ${damage} skada!`,
+            `Han träffar dig för ${damage} skada!`,
+            `Han stampar på dina tår för ${damage} skada!`
+        ]
+        
+        log(enemyattacks[Math.ceil(Math.random() * enemyattacks.length - 1)], "enemy")
         playerHp -= damage
     } else {
-        console.log("Snyggt parerat, inget händer!")
+        log("Inget hände!")
     }
 
-    playerHpElement.textContent = playerHp
-    enemyHpElement.textContent = enemyHp
+    if (playerHp < 1 || enemyHp < 1){
+        playButton.disabled = true
+    } else if (playerHp < 30) {
+        playerHpElement.classList.add("hp-low")
+    }
+
+    playerHpElement.textContent = playerHp < 1 ? 0 : playerHp
+    enemyHpElement.textContent = enemyHp < 1 ? 0 : enemyHp
 }
 
-const playButton = document.querySelector("#play-button")
+const combatLogElement = document.querySelector("#combat-log")
+
 playButton.addEventListener("click", gameRound)
