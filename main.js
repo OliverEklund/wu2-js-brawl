379ad03fec75e8c6bd3124b16841243408137b02
OliverEklund <oliver.eklund@elev.ga.ntig.se>
2025-11-26 13:39:29 +0100

    refactor: update enemy handling and improve game narrative

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const playerName = "Olle"
 let playerHp = 100
-let enemyHp = 100
+let enemyHp = 50
 
 const playButton = document.querySelector("#play-button")
 
@@ -10,11 +10,33 @@ function rollDice() {
 
 const playerHpElement = document.querySelector("#player-hp")
 const enemyHpElement = document.querySelector("#enemy-hp")
+const enemyNameElement = document.querySelector("#enemy-name")
 
 class Enemy {
-    constructor(name, hp, money)
-    this.name = hp
-    this.money = money
+    constructor(name, hp, money){
+    this.name = name
+    this.hp = hp
+    this.money = money}
+}
+
+function spawnEnemy() {
+    const enemychoices = [
+       new Enemy("Vätte", 30, 0),
+       new Enemy("Gnoll", 50, 0),
+       new Enemy("Orc", 80, 0),
+       new Enemy("Troll", 400, 0),
+       new Enemy("Skelett", 60, 0),
+       new Enemy("Skelett Riddare", 100, 0),
+       new Enemy("Nekromant", 200, 0),
+       new Enemy("Zombie", 40, 0),
+       new Enemy("Vampyr", 300, 0),
+       new Enemy("Stor Råtta", 10, 0),
+       new Enemy("Rått-person", 45, 0),
+       new Enemy("Rått-kungen", 600, 0),
+       new Enemy("Lindorm", 200, 0),
+       new Enemy("Drake", 1000, 0)
+    ]
+    return enemychoices[Math.floor(Math.random() * enemychoices.length)]
 }
 
 function log(message, type) {
@@ -28,6 +50,9 @@ function log(message, type) {
         combatLogElement.removeChild(combatLogElement.firstChild)
     }
 }
+
+let round
+let enemy = spawnEnemy()
 
 function gameRound() {    
     const playerRoll = rollDice()
@@ -44,7 +69,7 @@ function gameRound() {
         ]
         
         log(playerattacks[Math.ceil(Math.random() * playerattacks.length - 1)], "player")
-        enemyHp -= damage
+        enemy.hp -= damage
     } else if (enemyRoll > playerRoll) {
         const damage = enemyRoll - playerRoll
 
@@ -62,21 +87,32 @@ function gameRound() {
         log("Inget hände!")
     }
 
-    if (playerHp < 1 || enemyHp < 1){
-        playButton.disabled = true
-    } else if (playerHp < 30) {
-        playerHpElement.classList.add("hp-low")
+    if (playerHp < 1) {
+            playButton.disabled = true
+            log("Du Förlorade!")
     }
+        if (enemy.hp < 1) {
+            log("Du vann!")
+        const heal = Math.floor(Math.random() * 45 + 10)
+        log(`Du återhämtar dig och får tillbaka ${heal} hp!`, "player")
+        playerHp += heal
+        enemy = spawnEnemy()
+        log(`När du fortsätter djupare in i labyrinten så dyker en ${enemy.name} upp!`, "enemy")
+    }
+
 
     playerHpElement.textContent = playerHp < 1 ? 0 : playerHp
-    enemyHpElement.textContent = enemyHp < 1 ? 0 : enemyHp
+    enemyHpElement.textContent = enemy.hp < 1 ? 0 : enemy.hp
+    enemyNameElement.textContent = enemy.name < 1 ? 0 : enemy.name
 }
 
-function.gameloop(timestamp) {
+function gameLoop(timestamp) {
+    console.log(timestamp, last)
     if (timestamp >= last + 1000) {
         gameRound()
-        last = timestamp;
+        last = timestamp
     }
+
     round = window.requestAnimationFrame(gameloop)
 }
 
